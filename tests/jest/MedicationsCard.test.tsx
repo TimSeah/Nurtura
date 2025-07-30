0e2c2bfb5b29d57a3d80b0b0574abcf7818d133e
Hugo Chen <l.hugochen@gmail.com>
2025-07-30 17:11:39 +0800

    Added: MedicationsCard.test.tsx all PASSED

--- tests/jest/MedicationsCard.test.tsx
+++ tests/jest/MedicationsCard.test.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import MedicationsCard from "../../src/pages/healthMonitoring/components/medications/MedicationsCard";
 import { CareRecipient } from "../../src/types";
